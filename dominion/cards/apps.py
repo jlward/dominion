8dca008f932d0e74959da8257616c9a051d99fc9
Jason Ward <jason.ward@policystat.com>
2016-01-03 13:57:31 -0500

    refs #3: created the rest of the treasure cards.

--- dominion/cards/apps.py
+++ dominion/cards/apps.py
@@ -4,21 +4,40 @@ from django.db.models.signals import post_migrate
 from dominion.cards.models import Treasure
 
 
-def create_cards(sender, **kwargs):
+def create_treasure(name, fields):
     try:
         card = Treasure.objects.get(
-            name='copper',
+            name=name,
         )
     except Treasure.DoesNotExist:
         card = Treasure(
-            name='copper',
-            cost=0,
-            money_value=1,
+            name=name,
+            cost=fields['cost'],
+            money_value=fields['money_value'],
         )
     else:
-        card.cost = 0
-        card.money_value = 1
+        card.cost = fields['cost']
+        card.money_value = fields['money_value']
     card.save()
+
+
+def create_cards(sender, **kwargs):
+    cards = {
+        'copper': {
+            'cost': 0,
+            'money_value': 1,
+        },
+        'silver': {
+            'cost': 3,
+            'money_value': 2,
+        },
+        'gold': {
+            'cost': 6,
+            'money_value': 3,
+        },
+    }
+    for name, fields in cards.items():
+        create_treasure(name, fields)
 
 
 class CardAppConfig(AppConfig):
