c4ab642063a759fbb10c1df61983d4a16c5acf0b
Jason Ward <jason.ward@policystat.com>
2016-01-03 12:49:45 -0500

    Base project.

---
+++ dominion/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dominion project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dominion.settings")
+
+application = get_wsgi_application()
