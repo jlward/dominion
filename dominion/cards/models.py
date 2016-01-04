830065686754b7c990cd27006be3f07ccac1af76
Jason Ward <jason.ward@policystat.com>
2016-01-03 20:08:18 -0500

    refs #7: Give a nice name to cards.

--- dominion/cards/models.py
+++ dominion/cards/models.py
@@ -6,6 +6,9 @@ class Card(models.Model):
     cost = models.PositiveSmallIntegerField()
     count = models.PositiveSmallIntegerField(default=10)
 
+    def __str__(self):
+        return self.name.title()
+
 
 class Treasure(Card):
     money_value = models.PositiveSmallIntegerField()
@@ -13,3 +16,25 @@ class Treasure(Card):
 
 class Victory(Card):
     points = models.PositiveSmallIntegerField()
+
+
+class CardInstanceManager(models.Manager):
+    def create_for_card(self, card, game):
+        CardInstance.objects.bulk_create(
+            CardInstance(
+                card=card,
+                game=game,
+            ) for _ in range(card.count)
+        )
+
+
+class CardInstance(models.Model):
+    card = models.ForeignKey('cards.Card')
+    game = models.ForeignKey('games.Game')
+    deck = models.ForeignKey(
+        'decks.Deck',
+        blank=True,
+        null=True,
+    )
+
+    objects = CardInstanceManager()
