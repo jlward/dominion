1d6bc789eb2465875a30ee3aeda3c62df9151083
Jason Ward <jason.louard.ward@gmail.com>
2023-01-21 09:51:53 -0500

    Added relation.

--- players/models.py
+++ players/models.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 
 class Player(models.Model):
     handle = models.CharField(max_length=20)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
         return self.handle
