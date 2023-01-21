ecce094ff7820b92a53977c9eca19554dd16e8ae
Jason Ward <jason.louard.ward@gmail.com>
2023-01-21 10:56:45 -0500

    Added command to auto-create a few users.

---
+++ accounts/management/commands/create_initial_data.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+from players.models import Player
+
+
+class Command(BaseCommand):
+    help = 'Create the initial stuff needed to make the app work'
+
+    def create_user(self, email, handle):
+        is_superuser = False
+        is_staff = False
+        if handle is None:
+            is_superuser = True
+            is_staff = True
+
+        User = get_user_model()
+        user = User.objects.create(
+            email=email,
+            is_superuser=is_superuser,
+            is_staff=is_staff,
+        )
+        user.set_password('pw')
+        user.save()
+        if handle:
+            Player.objects.create(
+                handle=handle,
+                user=user,
+            )
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        users = [
+            ['staff@playdominion.com', None],
+            ['jason.louard.ward@gmail.com', 'Ward'],
+            ['derek.jamerson@gmail.com', 'Probably D'],
+        ]
+        for email, handle in users:
+            try:
+                User.objects.get(email=email)
+            except User.DoesNotExist:
+                self.create_user(email=email, handle=handle)
