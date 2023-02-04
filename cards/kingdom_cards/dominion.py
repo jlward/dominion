439151554a03df84c0e920483ad37d3f3dae9ab0
Jason Ward <jason.louard.ward@gmail.com>
2023-02-04 14:56:01 -0500

    list base cards

--- cards/kingdom_cards/dominion.py
+++ cards/kingdom_cards/dominion.py
@@ -96,3 +96,67 @@ class Chapel(Card):
             game=turn.game,
             card=self,
         )
+
+
+class Adventurer(Card):
+    pass
+
+
+class Bureaucrat(Card):
+    pass
+
+
+class Cellar(Card):
+    pass
+
+
+class Chancellor(Card):
+    pass
+
+
+class Feast(Card):
+    pass
+
+
+class Gardens(Card):
+    pass
+
+
+class Library(Card):
+    pass
+
+
+class Militia(Card):
+    pass
+
+
+class Mine(Card):
+    pass
+
+
+class Moat(Card):
+    pass
+
+
+class Moneylender(Card):
+    pass
+
+
+class Remodel(Card):
+    pass
+
+
+class Spy(Card):
+    pass
+
+
+class Thief(Card):
+    pass
+
+
+class ThroneRoom(Card):
+    pass
+
+
+class Workshop(Card):
+    pass
