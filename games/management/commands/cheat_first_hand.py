84cf636912cffbfda277a9bf48b842889448327c
Jason Ward <jason.louard.ward@gmail.com>
2023-02-18 13:27:59 -0500

    update mgmt command

--- games/management/commands/cheat_first_hand.py
+++ games/management/commands/cheat_first_hand.py
@@ -7,12 +7,17 @@ from players.models import Player
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('kingdom_cards', type=str, nargs='+')
+        parser.add_argument('--starting-money', type=int, default=0)
+        parser.add_argument('--starting-buys', type=int, default=1)
 
     def handle(self, *args, **options):
         kingdom_cards = options['kingdom_cards']
         players = Player.objects.all()
         game = Game.objects.create_game(players, kingdom_cards)
-        game.create_turn(players[0])
+        turn = game.create_turn(players[0])
+        turn.available_buys = options['starting_buys']
+        turn.available_money = options['starting_money']
+        turn.save()
         for deck in game.decks.all():
             deck.draw_pile = []
             deck.hand = kingdom_cards
