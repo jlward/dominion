565be3a9d4dfc75f4b962f515fa9b571f61426f5
Jason Ward <jason.louard.ward@gmail.com>
2023-03-04 15:19:19 -0500

    test turns/views

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from faker import Faker
 
+from games.models import Game
+from players.factories import PlayerFactory
+
 
 class BaseTestCase(TestCase):
     faker = Faker()
+
+    def create_game(self, kingdom_cards=None):
+        if kingdom_cards is None:
+            kingdom_cards = ['Village', 'Smithy']
+        players = PlayerFactory.create_batch(2)
+        game = Game.objects.create_game(players, kingdom_cards)
+        game.create_turn(players[0])
+        return game
