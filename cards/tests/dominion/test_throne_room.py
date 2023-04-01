31539665f08fc199b9e9adf681a2587e16e08b16
Jason Ward <jason.louard.ward@gmail.com>
2023-04-01 09:31:54 -0400

    Added tests for form.

--- cards/tests/dominion/test_throne_room.py
+++ cards/tests/dominion/test_throne_room.py
@@ -1,10 +1,10 @@
-# from unittest import mock
+from unittest import mock
 
 from cards.kingdom_cards.dominion import ThroneRoom
 from games.factories import GameFactory
 from players.factories import PlayerFactory
 from testing import BaseTestCase
-from turns.factories import TurnFactory
+from turns.factories import AdHocTurnFactory, TurnFactory
 
 
 class ThroneRoomCardTestCase(BaseTestCase):
@@ -28,4 +28,33 @@ class ThroneRoomCardTestCase(BaseTestCase):
             player=self.player,
             game=self.game,
             card=self.card,
+        )
+
+
+class ThroneRoomFormTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.player = PlayerFactory()
+        self.game = GameFactory(players=[self.player] + PlayerFactory.create_batch(11))
+        self.turn = TurnFactory(player=self.player, game=self.game)
+        self.deck = self.game.decks.get(player=self.player)
+        self.card = ThroneRoom()
+        self.adhoc_turn = AdHocTurnFactory(
+            player=self.player,
+            game=self.game,
+            turn=self.turn,
+            card=self.card,
+        )
+
+    def test_play_action_called_twice_when_calling_save(self):
+        form = self.build_card_form(adhoc_turn=self.adhoc_turn, cards=['Village'])
+        assert form.is_valid()
+        with mock.patch('turns.models.Turn.play_action') as play_action:
+            form.save()
+        self.assertEqual(play_action.call_count, 2)
+        play_action.assert_has_calls(
+            [
+                mock.call(mock.ANY, consume=False),
+                mock.call(mock.ANY, consume=False, ghost_action=True),
+            ],
         )
