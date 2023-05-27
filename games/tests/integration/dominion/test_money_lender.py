2493d24deeb1ee29f4cedfa300c3fd7952eef4b7
Jason Ward <jason.louard.ward@gmail.com>
2023-05-27 10:38:02 -0400

    Added tests for money lender.

---
+++ games/tests/integration/dominion/test_money_lender.py
@@ -0,0 +1,55 @@
+from ..testcases import IntegrationTestCase
+
+
+class MoneylenderTestCase(IntegrationTestCase):
+    player_starting_hand = ['Moneylender', 'Copper']
+
+    def test_trash(self):
+        self.assert_initial_state()
+
+        self.player_play_card('Moneylender')
+
+        self.assert_player_adhoc_turn_modal_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        self.player_pick_yes_no_from_modal(self.SELECTION_YES)
+
+        self.assert_player_adhoc_turn_modal_not_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        r = self.player_client.get(self.game_url)
+        self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=3))
+        self.assertCountEqual(self.get_player_hand(r), [])
+
+    def test_do_not_trash(self):
+        self.assert_initial_state()
+
+        self.player_play_card('Moneylender')
+
+        self.assert_player_adhoc_turn_modal_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        self.player_pick_yes_no_from_modal(self.SELECTION_NO)
+
+        self.assert_player_adhoc_turn_modal_not_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        r = self.player_client.get(self.game_url)
+        self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=0))
+        self.assertCountEqual(self.get_player_hand(r), ['Copper'])
+
+
+class MoneylenderNoCopperTestCase(IntegrationTestCase):
+    player_starting_hand = ['Moneylender', 'Silver']
+
+    def test(self):
+        self.assert_initial_state()
+
+        self.player_play_card('Moneylender')
+
+        self.assert_player_adhoc_turn_modal_not_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        r = self.player_client.get(self.game_url)
+        self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=0))
+        self.assertCountEqual(self.get_player_hand(r), ['Silver'])
