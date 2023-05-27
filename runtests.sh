6eb7a2f2d403cb7e778f0b1a6d8fca938fa3063c
Jason Ward <jason.louard.ward@gmail.com>
2023-05-27 09:04:45 -0400

    Run tests in parllel for great speed.

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-rm -rf .coverage
-coverage run ./manage.py test --settings=dominion.settings_tests "$@" -v 2 --noinput --buffer &&
+rm -rf .coverage.* .coverage
+coverage run ./manage.py test --settings=dominion.settings_tests "$@" -v 2 --noinput --buffer --parallel=8 &&
+coverage combine &&
 coverage report --show-missing --skip-covered &&
 echo "Success"
