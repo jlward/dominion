32f365838d4eee3013f3efecdac726347ea83b14
Jason Ward <jason.louard.ward@gmail.com>
2023-05-27 10:44:23 -0400

    Added another test for militia.

--- games/tests/integration/dominion/test_militia.py
+++ games/tests/integration/dominion/test_militia.py
@@ -5,7 +5,7 @@ class MilitiaTestCase(IntegrationTestCase):
     player_starting_hand = ['Militia', 'Silver', 'Gold', 'Smithy']
     opponent_starting_hand = ['Copper', 'Estate', 'Copper', 'Estate', 'Village']
 
-    def test_modal_pops_up_when_card_played(self):
+    def test(self):
         self.assert_initial_state()
         self.player_play_card('Militia')
 
@@ -21,3 +21,15 @@ class MilitiaTestCase(IntegrationTestCase):
         self.assert_your_turn(r)
         self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=2))
         self.assertCountEqual(self.get_oppnent_hand(r), ['Copper', 'Estate', 'Village'])
+
+
+class MilitiaOpponentShortCardsTestCase(IntegrationTestCase):
+    player_starting_hand = ['Militia', 'Silver', 'Gold', 'Smithy']
+    opponent_starting_hand = ['Copper', 'Estate', 'Village']
+
+    def test(self):
+        self.assert_initial_state()
+        self.player_play_card('Militia')
+
+        self.assert_player_adhoc_turn_modal_not_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
