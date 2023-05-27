0bf250b75ef8027a2249e08d425b0ed0cca9b4bf
Jason Ward <jason.louard.ward@gmail.com>
2023-05-27 09:40:44 -0400

    Added another test for chapel.

--- games/tests/integration/dominion/test_chapel.py
+++ games/tests/integration/dominion/test_chapel.py
@@ -1,10 +1,10 @@
 from ..testcases import IntegrationTestCase
 
 
-class ChapelTestCase(IntegrationTestCase):
+class ChapelWithHandTestCase(IntegrationTestCase):
     player_starting_hand = ['Chapel', 'Silver', 'Gold', 'Smithy']
 
-    def test_modal_pops_up_when_card_played(self):
+    def test(self):
         self.assert_initial_state()
         self.player_play_card('Chapel')
 
@@ -19,3 +19,18 @@ class ChapelTestCase(IntegrationTestCase):
         r = self.player_client.get(self.game_url)
         self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=0))
         self.assertCountEqual(self.get_player_hand(r), ['Smithy'])
+
+
+class ChapelWithoutHandTestCase(IntegrationTestCase):
+    player_starting_hand = ['Chapel']
+
+    def test(self):
+        self.assert_initial_state()
+        self.player_play_card('Chapel')
+
+        self.assert_player_adhoc_turn_modal_not_present()
+        self.assert_opponent_adhoc_turn_modal_not_present()
+
+        r = self.player_client.get(self.game_url)
+        self.assertEqual(self.get_resources(r), dict(actions=0, buys=1, money=0))
+        self.assertCountEqual(self.get_player_hand(r), [])
